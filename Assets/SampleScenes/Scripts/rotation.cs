d73da43fab17c3d41db76dcaef70502f48068b9e
zoo300 <zoo300@zoo300deMacBook-Pro.local>
2019-02-28 21:47:55 -0600

    Added rotation script

---
+++ Assets/SampleScenes/Scripts/rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float howFast = 2.0f; // modify the value to match the needed speed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * howFast, 0, 0);
+
+
+    }
+}
---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    float m_speed;
+    public bool m_worldSpace;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_speed = 200.0f;
+        transform.Rotate(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_worldSpace)
+            transform.Rotate(Vector3.up * m_speed * Time.deltaTime, Space.World);
+        else
+            transform.Rotate(Vector3.up * m_speed * Time.deltaTime, Space.Self);
+    }
+}
